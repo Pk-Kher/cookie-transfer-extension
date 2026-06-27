53413d51c81942030ca163f5677617e8fef622e1
pk-kher <pradipkher3@gmail.com>
2026-06-27 12:57:37 +0530

    change the min version of the browser

--- scripts/build.js
+++ scripts/build.js
@@ -25,7 +25,7 @@ const MANIFESTS = {
     browser_specific_settings: {
       gecko: {
         id: 'cookie-transfer@extension',
-        strict_min_version: '109.0',
+        strict_min_version: '128.0',
         data_collection_permissions: {
           required: ['none'],
         },
